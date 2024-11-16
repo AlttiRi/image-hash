23ab89d280a0beea39d4ac7e698d01749c587854
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-11-16 11:35:45 +0300

    Update tests

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,1 @@
-import {Tester} from "@alttiri/util-node-js";
-
-
-const {t} = new Tester().destructible();
-
-t({
-    expect: 4,
-    result: 2 + 2,
-    name: "Example 2 + 2",
-});
+import "./util/example.ts";
