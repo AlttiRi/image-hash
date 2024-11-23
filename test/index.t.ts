de5771ce2297047f3e147753a7aca33c1347aecc
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-11-23 10:38:55 +0300

    Rename test files

---
+++ test/index.t.ts
@@ -0,0 +1,6 @@
+import "./image-hash/bin-to-bi-array.t.ts";
+import "./image-hash/biui8a-to-bin.t.ts";
+import "./image-hash/biui8a-to-hex.t.ts";
+import "./image-hash/class-methods.t.ts";
+import "./image-hash/hex-to-bi-array.t.ts";
+import "./image-hash/distance-biui8a.t.ts";
