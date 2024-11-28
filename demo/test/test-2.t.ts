7e556a8fce157aea63c58d1628692790677eb8e6
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-11-28 20:49:07 +0300

    Add `grayScaler` tests

--- demo/test/test-2.t.ts
+++ demo/test/test-2.t.ts
@@ -1,4 +1,59 @@
 import {ANSI_BLUE, t} from "../../test/tester.ts";
+import {ImageHash} from "@/image-hash.ts";
+import {dHash} from "@/hashers.ts";
+import {readFileImageData} from "../util.demo.ts";
+import path from "node:path";
+import {getCalculateAverage, getCalculateBT601, getCalculateBT709} from "@/grayscale.ts";
+function resolve(str: string) {
+    return path.resolve(import.meta.dirname, str);
+}
 
 console.log(ANSI_BLUE("--- Tests 2 ---"));
 
+const iData = await readFileImageData(resolve(`../img/wallpaper-dark-purple-2560x1600.jpg`));
+const hex1_c4e4f05879797e3e = "c4e4f05879797e3e";
+const hex2_c4e4f15879797e3e = "c4e4f15879797e3e";
+const hex3_c4e4f05879793e3a = "c4e4f05879793e3a";
+
+{
+    const hash = dHash(iData);
+    t({
+        result: hash.hex,
+        expect: hex1_c4e4f05879797e3e,
+    });
+}
+{
+    const hash = dHash(iData, {grayScaler: getCalculateBT601});
+    t({
+        result: hash.hex,
+        expect: hex1_c4e4f05879797e3e,
+    });
+}
+
+{
+    const hash = dHash(iData, {grayScaler: getCalculateAverage});
+    t({
+        result: hash.hex,
+        expect: hex2_c4e4f15879797e3e,
+    });
+    t({
+        result: hash.diff(ImageHash.fromHex(hex1_c4e4f05879797e3e)),
+        expect: 1,
+    });
+}
+
+{
+    const hash = dHash(iData, {grayScaler: getCalculateBT709});
+    t({
+        result: hash.hex,
+        expect: hex3_c4e4f05879793e3a,
+    });
+    t({
+        result: hash.diff(ImageHash.fromHex(hex1_c4e4f05879797e3e)),
+        expect: 2,
+    });
+    t({
+        result: hash.diff(ImageHash.fromHex(hex2_c4e4f15879797e3e)),
+        expect: 3,
+    });
+}
