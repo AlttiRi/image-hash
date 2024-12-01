47654c3f3151a842b6feb9a77c0270fda306aa4a
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-12-01 13:20:00 +0300

    Add `_mHashCoreClassic`

--- src/hashers-core.ts
+++ src/hashers-core.ts
@@ -23,21 +23,34 @@ export function aHashCore({data, width, height}: GrayImageData): BiImageData {
     return new BiImageData(hash, width, height);
 }
 
+// todo: use it optionally
+export function _mHashCoreClassic(data: Uint8Array, width: number, height: number): Uint8Array {
+    const median = calculateMedian(data);
+    const hash = new Uint8Array(width * height);
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            hash[y * width + x] = data[y * width + x] > median ? 255 : 0;
+        }
+    }
+    return hash;
+}
+
 export function mHashCore({data, width, height}: GrayImageData): BiImageData {
     const hash = _mHashCore(data, width, height);
     return new BiImageData(hash, width, height);
 }
 
-// todo: add tests
 /**
- * Two conditions for the proper handling of images with extreme median values (~0 or ~255).
+ * Two conditions for the proper handling of images with extreme median values (~0 or ~255). Do not edit.
  *
- * Do not edit.
+ * On dark  images the median value will be a black pixel.
+ * On light images the median value will be a white pixel.
  *
- * Check the "big" hashes for
- * - `black-bg-orthocanna-500x500` and
+ * So, it prefers to display the light details on dark images and vice versa.
+ *
+ * Check the "big" hashes for the follow images:
+ * - `black-bg-orthocanna-500x500`
  * - `screenshot-dark-purple-flower-1353x851` (look at the text on the screenshot)
- * images.
  */
 function _mHashCore(data: Uint8Array, width: number, height: number): Uint8Array {
     const median = calculateMedian(data);
