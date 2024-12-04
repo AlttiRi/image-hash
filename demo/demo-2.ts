10a7e222ec62a32a3ff510e8214aa24427a892f8
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-12-04 22:54:26 +0300

    Add demo 2

---
+++ demo/demo-2.ts
@@ -0,0 +1,38 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import {saveImageDataWithSharp} from "./util.demo.ts";
+import {getGrayData} from "@/grayscale.ts";
+import {scaleDownLinear} from "@/resize.ts";
+import {aHash, bHash, dHash, mHash} from "@/hashers.ts";
+import {Files, getImageData} from "./constants.ts";
+
+const dirPath = "./demo-img-out/demo-2";
+await fs.mkdir(dirPath, {recursive: true});
+
+const size = 256;
+for (const filename of Object.values(Files)) {
+
+    const iData = await getImageData(filename);
+    const grayData       = getGrayData(iData);
+    const grayDataScaled = scaleDownLinear(grayData, {size, ignore: true});
+    const a_hash = aHash(iData, {size, grayData, grayDataScaled});
+    const m_hash = mHash(iData, {size, grayData, grayDataScaled});
+    const d_hash = dHash(iData, {size, grayData, grayDataScaled, ignore: true});
+    const b_hash = bHash(iData, {size, grayData, grayDataScaled});
+
+    await saveImageDataWithSharp(a_hash.mono, getPath({filename, size, prefix: "avg"}));
+    await saveImageDataWithSharp(m_hash.mono, getPath({filename, size, prefix: "med"}));
+    await saveImageDataWithSharp(d_hash.mono, getPath({filename, size, prefix: "dif"}));
+    await saveImageDataWithSharp(b_hash.mono, getPath({filename, size, prefix: "blk"}));
+}
+
+
+function getPath({filename, prefix, size}: GetPathParams) {
+    return path.join(dirPath,`${size}-${path.basename(filename)}-${prefix}.png`);
+}
+
+type GetPathParams = {
+    filename: string
+    prefix: string
+    size: number
+};
