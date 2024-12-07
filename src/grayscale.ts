919d29934a6b9c63d8f9c15d7745b5c4520f91d1
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-12-07 23:06:26 +0300

    Use `littleEndian` in `getUint32`

--- src/grayscale.ts
+++ src/grayscale.ts
@@ -21,10 +21,10 @@ function isString(value: unknown): value is string {
 export const getCalculateBT601 = getCalculateBT601_Original;
 export function getCalculateBT601_Original(dw: DataView) {
     return function calculateBT601(i: number) {
-        const uint = dw.getUint32(i);
-        return ((uint >> 24) & 0xFF) * 0.299
-            +  ((uint >> 16) & 0xFF) * 0.587
-            +  ((uint >>  8) & 0xFF) * 0.114;
+        const uint = dw.getUint32(i, true);
+        return ( uint        & 0xFF) * 0.299
+            +  ((uint >>  8) & 0xFF) * 0.587
+            +  ((uint >> 16) & 0xFF) * 0.114;
          // + 0.5 << 0; // round
     };
 }
@@ -33,27 +33,27 @@ export function getCalculateBT601_Original(dw: DataView) {
  * (only 0.05 % difference no more than by 1 in comparison with the rounded version). */
 export function getCalculateBT601_Optimized(dw: DataView) {
     return function calculateBT601(i: number) {
-        const uint = dw.getUint32(i);
-        return (((uint >> 24) & 0xFF) * 19595
-            +   ((uint >> 16) & 0xFF) * 38470
-            +   ((uint >>  8) & 0xFF) *  7471
+        const uint = dw.getUint32(i, true);
+        return ((uint        & 0xFF) * 19595
+            +  ((uint >>  8) & 0xFF) * 38470
+            +  ((uint >> 16) & 0xFF) *  7471
             + 0x8000) >> 16; // "0x8000" === "1 << 15" === 32768
     };
 }
 export function getCalculateAverage(dw: DataView) {
     return function calculateAverage(i: number) {
-        const uint = dw.getUint32(i);
-        return (((uint >> 24) & 0xFF)
-             +  ((uint >> 16) & 0xFF)
-             +  ((uint >>  8) & 0xFF)) / 3;
+        const uint = dw.getUint32(i, true);
+        return ((uint        & 0xFF)
+             + ((uint >>  8) & 0xFF)
+             + ((uint >> 16) & 0xFF)) / 3;
     };
 }
 export function getCalculateBT709(dw: DataView) {
     return function calculateBT709(i: number) {
-        const uint = dw.getUint32(i);
-        return ((uint >> 24) & 0xFF) * 0.2126
-            +  ((uint >> 16) & 0xFF) * 0.7152
-            +  ((uint >>  8) & 0xFF) * 0.0722;
+        const uint = dw.getUint32(i, true);
+        return ( uint        & 0xFF) * 0.2126
+            +  ((uint >>  8) & 0xFF) * 0.7152
+            +  ((uint >> 16) & 0xFF) * 0.0722;
     };
 }
 
@@ -82,7 +82,6 @@ export function getGrayData(imageData: ImageDataLike, getFunc: GrayScalingOpt = 
     const {data, width, height, data: {length}} = imageData;
     const array = new Uint8Array(length / 4);
 
-    // todo: use Uint32Array // (uint & 0xFF) * 0.299 + ((uint >> 8) & 0xFF) * 0.587 + ((uint >>  16) & 0xFF) * 0.114;
     const dw = new DataView(data.buffer);
     const calculateLuminance = getFunc(dw);
 
@@ -93,3 +92,16 @@ export function getGrayData(imageData: ImageDataLike, getFunc: GrayScalingOpt = 
     }
     return new GrayImageData(array, width, height);
 }
+
+// todo: use Uint32Array
+//
+//     const u32 = new Uint32Array(data.buffer);
+//     const l32 = u32.length;
+//     for (let i = 0; i < l32; i += 1) {
+//         const uint = u32[i];
+//         array[i] = (
+//                  (uint        & 0xFF) * 19595
+//             +   ((uint >>  8) & 0xFF) * 38470
+//             +   ((uint >> 16) & 0xFF) *  7471
+//             + 0x8000) >> 16;
+//     }
